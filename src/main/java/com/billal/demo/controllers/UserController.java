8d6578039dc55ee6d2b1b1e225cf19ebd6d45829
ELMAALMI <tcsbilal@gmail.com>
2024-02-19 15:31:12 +0100

    first commit

--- src/main/java/com/billal/demo/controllers/UserController.java
+++ src/main/java/com/billal/demo/controllers/UserController.java
@@ -1,12 +1,27 @@
 package com.billal.demo.controllers;
 
+import com.billal.demo.models.User;
 import com.billal.demo.servcices.UserService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
     private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> saveUser(@RequestBody User user) {
+        return ResponseEntity.ok(this.service.saveUser(user));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> get() {
+        return ResponseEntity.ok(this.service.getAllUsers());
+    }
 
 }
